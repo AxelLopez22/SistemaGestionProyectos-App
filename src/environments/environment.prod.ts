687ee882cea0f09c6165ff13619409a42b5955fb
Axel Lopez <axellopezbrizuela3@gmail.com>
2023-09-23 04:53:02 -0600

    cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://apiprojectmanagement-prod.us-east-2.elasticbeanstalk.com/api/',
+  baseUrl: 'https://www.gestion-proyectos.somee.com/api/',
   //baseUrl: 'https://localhost:7015/api/',
-  baseUrlHub: 'http://apiprojectmanagement-prod.us-east-2.elasticbeanstalk.com'
+  baseUrlHub: 'https://www.gestion-proyectos.somee.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://apiprojectmanagement-prod.us-east-2.elasticbeanstalk.com/api/',
-  //baseUrl: 'https://localhost:7015/api/',
-  baseUrlHub: 'http://apiprojectmanagement-prod.us-east-2.elasticbeanstalk.com'
-  //baseUrlHub: 'https://localhost:7015'
+  //baseUrl: 'https://www.gestion-proyectos.somee.com/api/',
+  baseUrl: 'https://localhost:7015/api/',
+  //baseUrlHub: 'https://www.gestion-proyectos.somee.com'
+  baseUrlHub: 'https://localhost:7015'
 };
 
 /*
